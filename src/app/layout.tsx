9c72c801966086f799d6eb87002c2a439cb3c868
Nagateja Valiveti <teja91.valiveti@gmail.com>
2024-06-25 21:15:19 +0530

    meta scripts added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import type { Metadata } from "next";
 
 // Components
@@ -30,6 +31,56 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <Navbar/>
         <main className="app__main">{children}</main>
         <Footer/>
+
+        {/* Microsoft Clarity - Business Analysis Tool */}
+
+        <Script id="clarity-script" strategy="afterInteractive">
+          {
+            `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a] || function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${process.env.MICROSOFT_CLARITY_ID}");
+            `
+          }
+        </Script>
+
+        {/* Google Tag */}
+        
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TAG_ID}`} async strategy="afterInteractive" />
+        <Script id="clarity-script" strategy="afterInteractive">
+          {
+            `
+            window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              
+              gtag('config', "${process.env.GOOGLE_TAG_ID}");
+              `
+          }
+        </Script>
+
+        {/* Meta Pixel */}
+        
+        <Script id="clarity-script" strategy="afterInteractive">
+          {
+            `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', "${process.env.META_PIXEL_ID}");
+              fbq('track', 'PageView');
+            `
+          }
+        </Script>
+        <Script src={`https://www.facebook.com/tr?id=${process.env.META_PIXEL_ID}&ev=PageView&noscript=1`} noModule></Script>
+
       </body>
     </html>
   )
